cf0682ff831f1a41d335a919c40a853e7483f56c
Neftali Valdez Calderon <neftalivaldez1@gmail.com>
2019-02-06 16:54:57 -0500

    completed basic functionality of the flash card

--- arrays.js
+++ arrays.js
@@ -1,35 +1,115 @@
 // Array with questions and answers for the HTML section
-let htmlInfo = [
-    {
-        question: 'Q1. What is HTML?',
-        answer: 'A1. HTML (HyperText Markup Language) is the most basic building block of the Web.'
-    },
+// let arrayInfo = [
+//     {
+//         question: 'What is HTML?',
+//         answer: 'HTML (HyperText Markup Language) is the most basic building block of the Web.',
+//         tag: 'HTML'
+//     },
 
-    {
-        question: 'Q2. What does HyperText refer to?',
-        answer: 'A2. HyperText" refers to links that connect web pages to one another, either within a single website or between websites.'
-    },
+//     {
+//         question: 'What does HyperText refer to?',
+//         answer: 'HyperText" refers to links that connect web pages to one another, either within a single website or between websites.',
+//         tag: 'HTML'
+//     },
 
-    {
-        question: 'Q3. The HTML <head>',
-        answer: 'A3. The HTML <head> element provides general information (metadata) about the document, including its title and links to its scripts and style sheets.'
-    }
-];
+//     {
+//         question: 'The HTML <head>',
+//         answer: 'The HTML <head> element provides general information (metadata) about the document, including its title and links to its scripts and style sheets.',
+//         tag: 'HTML'
+//     },
 
-let cssInfo = [
-    {
-        question: 'Q1. What is CSS?',
-        answer: 'A1. Cascading Style Sheets (CSS) is a stylesheet language used to describe the presentation of a document written in HTML or XML.'
-    },
+//     {
+//         question: 'What is CSS?',
+//         answer: 'Cascading Style Sheets (CSS) is a stylesheet language used to describe the presentation of a document written in HTML or XML.',
+//         tag: 'CSS'
+//     },
 
-    {
-        question: 'Q2. What is a CCS Selector?',
-        answer: 'A2. A CSS Selector is the HTML element name at the start of the rule set. It selects the element(s) to be styled.'
-    },
+//     {
+//         question: 'What is a CCS Selector?',
+//         answer: 'A CSS Selector is the HTML element name at the start of the rule set. It selects the element(s) to be styled.',
+//         tag: 'CSS'
+//     },
 
-    {
-        question: 'Q3. What is a CSS Declaration?',
-        answer: 'A3. A single rule like color: red; specifying which of the element\'s properties you want to style.'
-    }
-];
+//     {
+//         question: 'What is a CSS Declaration?',
+//         answer: 'A single rule like color: red; specifying which of the element\'s properties you want to style.',
+//         tag: 'CSS'
+//     }
+// ];
 
+// let cssInfo = [
+//     {
+//         question: 'Q1. What is CSS?',
+//         answer: 'A1. Cascading Style Sheets (CSS) is a stylesheet language used to describe the presentation of a document written in HTML or XML.',
+//         tag: 'CSS'
+//     },
+
+//     {
+//         question: 'Q2. What is a CCS Selector?',
+//         answer: 'A2. A CSS Selector is the HTML element name at the start of the rule set. It selects the element(s) to be styled.',
+//         tag: 'CSS'
+//     },
+
+//     {
+//         question: 'Q3. What is a CSS Declaration?',
+//         answer: 'A3. A single rule like color: red; specifying which of the element\'s properties you want to style.',
+//         tag: 'CSS'
+//     }
+// ];
+
+
+// grabs the CSS button from the DOM
+// let cssQuestion = document.getElementById('css-section');
+
+
+// let instructionCSS = 1;
+
+// add instructions when the "HTML" buttom is clicked
+// cssQuestion.addEventListener('click', function () {
+
+//     if (instructionCSS === 1) {
+
+//         let node = document.createElement("p");
+//         let text = document.createTextNode('Please click on \"Next Question\" to begin.')
+//         node.appendChild(text)
+//         document.getElementById("div-question").appendChild(node);
+//         instructionCSS++;
+//     }
+// });
+
+// show the next question when "Next Question" buttom is click
+// let questionCSS = document.getElementById('question');
+
+// let counterQuestionCSS = 0;
+// question.addEventListener('click', function(){
+
+//      if (counterQuestionCSS < cssInfo.length) {
+
+//         let node = document.createElement("p");
+//         let text = document.createTextNode(cssInfo[counterQuestionCSS].question);
+//         node.appendChild(text)
+//         document.getElementById("div-question").appendChild(node);
+
+//         counterQuestionCSS++;
+//     }
+
+// })
+
+// grabs the "Show Answer" buttom from the DOM
+// let cssAnswer = document.getElementById('answer');
+
+// let cssCounterAnswer = 0;
+
+// show the next answer when "Show Answer" buttom is click
+// cssAnswer.addEventListener('click', function () {
+
+//     if (cssCounterAnswer < htmlInfo.length) {
+
+//         let node = document.createElement("p");
+//         let text = document.createTextNode(htmlInfo[cssCounterAnswer].answer);
+//         node.appendChild(text)
+//         document.getElementById("div-answer").appendChild(node);
+
+//         cssCounterAnswer++;    
+// }
+// });
--- script.js
+++ script.js
@@ -1,54 +1,116 @@
+let arrayInfo = [
+    {
+        question: 'What is HTML?',
+        answer: 'HTML (HyperText Markup Language) is the most basic building block of the Web.',
+        tag: 'HTML'
+    },
+
+    {
+        question: 'What does HyperText refer to?',
+        answer: 'HyperText" refers to links that connect web pages to one another, either within a single website or between websites.',
+        tag: 'HTML'
+    },
+
+    {
+        question: 'The HTML <head>',
+        answer: 'The HTML <head> element provides general information (metadata) about the document, including its title and links to its scripts and style sheets.',
+        tag: 'HTML'
+    },
+
+    {
+        question: 'What is CSS?',
+        answer: 'Cascading Style Sheets (CSS) is a stylesheet language used to describe the presentation of a document written in HTML or XML.',
+        tag: 'CSS'
+    },
+
+    {
+        question: 'What is a CCS Selector?',
+        answer: 'A CSS Selector is the HTML element name at the start of the rule set. It selects the element(s) to be styled.',
+        tag: 'CSS'
+    },
+
+    {
+        question: 'What is a CSS Declaration?',
+        answer: 'A single rule like color: red; specifying which of the element\'s properties you want to style.',
+        tag: 'CSS'
+    }
+];
 
 // grabs the HTML button from the DOM
-let htmlQuestion = document.getElementById('html-section');
+// let htmlQuestion = document.getElementById('html-section');
 
-let counterQuestion = 0;
-let instruction = 1;
+let countQuestion = 0;
+// let instruction = 1;
 
 // add instructions when the "HTML" buttom is clicked
-htmlQuestion.addEventListener('click', function () {
+// htmlQuestion.addEventListener('click', function () {
 
-    if (instruction === 1) {
+//     if (instruction === 1) {
 
-        let node = document.createElement("p");
-        let text = document.createTextNode('Please click on next question to begin.')
-        node.appendChild(text)
-        document.getElementById("div-question").appendChild(node);
-        instruction++;
-    }
-});
+//         let node = document.createElement("p");
+//         let text = document.createTextNode('Please click on \"Next Question\" to begin.')
+//         node.appendChild(text)
+//         document.getElementById("div-question").appendChild(node);
+//         instruction++;
+//     }
+// });
 
 // show the next question when "Next Question" buttom is click
 let question = document.getElementById('question');
-question.addEventListener('click', function(){
+question.addEventListener('click', function () {
 
-     if (counterQuestion < htmlInfo.length) {
+    document.getElementById("div-question").innerHTML = "";
 
-        let node = document.createElement("p");
-        let text = document.createTextNode(htmlInfo[counterQuestion].question);
-        node.appendChild(text)
-        document.getElementById("div-question").appendChild(node);
+    if (countQuestion === arrayInfo.length) {
 
-        counterQuestion++;
+        const message = document.createElement('p');
+        const messageText = document.createTextNode('Thank you for participating!')
+        message.appendChild(messageText);
+        document.getElementById("div-question").appendChild(message);
     }
 
+    if (countQuestion < arrayInfo.length) {
+
+        let node = document.createElement("p");
+        let text = document.createTextNode(arrayInfo[countQuestion].question);
+        node.appendChild(text);
+        document.getElementById("div-question").appendChild(node);
+
+        countQuestion++;
+    }
 })
 
 // grabs the "Show Answer" buttom from the DOM
 let htmlAnswer = document.getElementById('answer');
 
-let counterAnswer = 0;
+let countAnswer = 0;
 
 // show the next answer when "Show Answer" buttom is click
 htmlAnswer.addEventListener('click', function () {
 
-    if (counterAnswer < htmlInfo.length) {
+    document.getElementById("div-answer").innerHTML = "";
+
+    if (countAnswer === arrayInfo.length) {
+
+        const message = document.createElement('p');
+        const messageText = document.createTextNode('Thank you for participating!')
+        message.appendChild(messageText);
+        document.getElementById("div-answer").appendChild(message);
+    }
+
+    if (countAnswer < arrayInfo.length) {
 
         let node = document.createElement("p");
-        let text = document.createTextNode(htmlInfo[counterAnswer].answer);
-        node.appendChild(text)
+        let text = document.createTextNode(arrayInfo[countAnswer].answer);
+        node.appendChild(text);
         document.getElementById("div-answer").appendChild(node);
 
-        counterAnswer++;    
-}
+        countAnswer++;
+    }
 });
+
+let htmlQuestions = arrayInfo.filter(list => {
+    return list.tag === 'HTML';
+})
+
+
