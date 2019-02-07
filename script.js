87e6e9351008ed0426f7dc761976b031523ed4df
Neftali Valdez Calderon <neftalivaldez1@gmail.com>
2019-02-07 11:54:24 -0500

    added event listener to the answers

--- script.js
+++ script.js
@@ -1,21 +1,16 @@
 let arrayInfo = [
     {
         question: 'What is HTML?',
-        answer: ['HTML (HyperText Markup Language) is the most basic building block of the Web.', 'test1',
-            'another test1', 'final test1'],
         tag: 'HTML'
     },
 
     {
         question: 'What does HyperText refer to?',
-        answer: ['HyperText" refers to links that connect web pages to one another, either within a single website or between websites.', 'test2',
-            'another test2', 'final test2'],
         tag: 'HTML'
     },
 
     {
         question: 'The HTML <head>',
-        answer: 'The HTML <head> element provides general information (metadata) about the document, including its title and links to its scripts and style sheets.',
         tag: 'HTML'
     },
 
@@ -42,16 +37,19 @@ let answers = [
     {
         answer: ['HTML (HyperText Markup Language) is the most basic building block of the Web.', 'test1',
             'another test1', 'final test1'],
+        correctAnswerIndex: 0
     },
 
     {
         answer: ['HyperText" refers to links that connect web pages to one another, either within a single website or between websites.', 'test2',
             'another test2', 'final test2'],
+        correctAnswerIndex: 1
     },
 
     {
         answer: ['The HTML <head> element provides general information (metadata) about the document, including its title and links to its scripts and style sheet', 'test3',
-        'another test3', 'final test3']
+            'another test3', 'final test3'],
+            correctAnswerIndex: 2
     }
 ];
 
@@ -78,17 +76,19 @@ let countQuestion = 0;
 let question = document.getElementById('question');
 question.addEventListener('click', function () {
 
+    // resetting the divs everytime some clicks the button
     document.getElementById("div-question").innerHTML = "";
     document.getElementById("div-answer").innerHTML = "";
 
+    // creating a message when there are not any question left
     if (countQuestion === arrayInfo.length) {
-
         const message = document.createElement('p');
         const messageText = document.createTextNode('Thank you for participating!')
         message.appendChild(messageText);
         document.getElementById("div-question").appendChild(message);
     }
 
+    // adding one question at the time when someone clicks "Next Question"
     if (countQuestion < arrayInfo.length) {
 
         let node = document.createElement("p");
@@ -96,50 +96,83 @@ question.addEventListener('click', function () {
         node.appendChild(text);
         document.getElementById("div-question").appendChild(node);
 
-            for (let j = 0; j < 4; j++){
+        // for loop that creates a button for every possible answer for each question
+        for (let j = 0; j < 4; j++) {
 
-                let node = document.createElement("p");
-                let text = document.createTextNode(answers[countQuestion].answer[j]);
-                node.appendChild(text);
-                document.getElementById("div-answer").appendChild(node);
+            let node = document.createElement("a");
+            node.setAttribute('href', '#');
+            node.setAttribute('class', 'button-answer')
+            node.setAttribute('data-index', j)
+            let text = document.createTextNode(answers[countQuestion].answer[j]);
+            node.appendChild(text);
+            document.getElementById("div-answer").appendChild(node);
 
-            }
-            
+        }
+        // coiunter to move through the indexes of the arrays
         countQuestion++;
     }
 })
 
-// grabs the "Show Answer" buttom from the DOM
-let htmlAnswer = document.getElementById('answer');
+let divAnswer = document.getElementById('div-answer');
 
-let countAnswer = 0;
+divAnswer.addEventListener('click', function (evt) {
 
-// show the next answer when "Show Answer" buttom is click
-htmlAnswer.addEventListener('click', function () {
+    // MOVE OUTSIDE OF THE OTHER EVENT LISTENER!!!!!!!!
+    // add event listenter to parent so when use clicks on a button (that we just created), check/validate the answer
 
-    document.getElementById("div-answer").innerHTML = "";
+    // step one: define Event Target, add Event Listener
+    // Event Handler: check Event Object to see if the target (evt.target) has the .button-answer class
 
-    if (countAnswer === arrayInfo.length) {
-
-        const message = document.createElement('p');
-        const messageText = document.createTextNode('Thank you for participating!')
-        message.appendChild(messageText);
-        document.getElementById("div-answer").appendChild(message);
+    for (let i = 0; i < answers.length; i++) {
+        
+        if (evt.target.getAttribute('data-index') == answers[i].correctAnswerIndex[i]) {
+            console.log('correct');
+        }
+        else {
+            console.log('wrong');
+        }
+        
     }
 
-    if (countAnswer < arrayInfo.length) {
+    // then, check the data-index to see if it matches the index of the correct answer
+    // then handle correct/incorrect answers appropriately
 
-        let node = document.createElement("p");
-        let text = document.createTextNode(arrayInfo[countAnswer].answer);
-        node.appendChild(text);
-        document.getElementById("div-answer").appendChild(node);
-
-        countAnswer++;
-    }
-});
-
-let htmlQuestions = arrayInfo.filter(list => {
-    return list.tag === 'HTML';
+    // console.log(evt.target.classList.contains('button-answer'))  
 })
 
+
+
+
+// grabs the "Show Answer" buttom from the DOM
+// let htmlAnswer = document.getElementById('answer');
+
+// let countAnswer = 0;
+
+// show the next answer when "Show Answer" buttom is click
+// htmlAnswer.addEventListener('click', function () {
+
+//     document.getElementById("div-answer").innerHTML = "";
+
+//     if (countAnswer === arrayInfo.length) {
+
+//         const message = document.createElement('p');
+//         const messageText = document.createTextNode('Thank you for participating!')
+//         message.appendChild(messageText);
+//         document.getElementById("div-answer").appendChild(message);
+//     }
+
+//     if (countAnswer < arrayInfo.length) {
+
+//         let node = document.createElement("p");
+//         let text = document.createTextNode(arrayInfo[countAnswer].answer);
+//         node.appendChild(text);
+//         document.getElementById("div-answer").appendChild(node);
+
+//         countAnswer++;
+//     }
+// });
+
+// let htmlQuestions = arrayInfo.filter(list => {
+//     return list.tag === 'HTML';
+// })
 
